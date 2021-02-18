dc86844b43397ae4c37e316057481941984a2fe3
Andrii Cherpak <andriycherpak.education@gmail.com>
2021-02-18 17:10:25 +0200

    Default directory added, all functions imported

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from Lab2_getMatrix import get_matrix
 from Lab2_Outp import outp
+from aaaaa import GetWinners
 
-dir = input("Enter the directory: ")
-table = get_matrix(dir)
+directory = input("Enter the directory: ")
+if directory == None: directory = "C:\\Users\\User\Desktop\Навчання\ОП\Lab_2\\"
+table = get_matrix(directory)
 rating = [(l[0], 0) for l in table]
+GetWinners(rating, table)
+outp(rating)
