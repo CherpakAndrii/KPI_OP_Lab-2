dac4afc11ccc669260b584a82190bfc5aaa045be
Andrii Cherpak <andriycherpak.education@gmail.com>
2021-02-18 16:34:38 +0200

    ок, як і просив, тепер rating - масив пар.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from Lab2_getMatrix import get_matrix, get_dict
+from Lab2_getMatrix import get_matrix
+from Lab2_Outp import outp
 
 dir = input("Enter the directory: ")
 table = get_matrix(dir)
-rating = [0]*len(table)
+rating = [(l[0], 0) for l in table]
