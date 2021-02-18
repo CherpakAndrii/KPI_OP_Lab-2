f0cc71e793baf10e429f8c3e2adb7ab09b4f15fb
Andrii Cherpak <andriycherpak.education@gmail.com>
2021-02-18 15:50:50 +0200

    a function that process files in given directory and returns 2D-array

---
+++ Lab2_getMatrix.py
@@ -0,0 +1,11 @@
+import glob
+
+def get_matrix(directory):
+    lst, table = [], []
+    files = glob.glob('{}*.csv'.format(directory))
+    for file in files:
+        f = open(file, "r")
+        lst += f.readlines()[1:]
+    for line in lst:
+        table.append(line.split(','))
+    return table
