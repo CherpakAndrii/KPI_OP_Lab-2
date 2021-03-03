3d1825949d62de58ff96bf32355d503710f32494
Andrew <andriycherpak.education@gmail.com>
2021-03-03 09:56:08 +0200

    checking for country name with '"' or ','

--- Lab2_getMatrix.py
+++ Lab2_getMatrix.py
@@ -1,14 +1,22 @@
 import glob
 
 def get_matrix(directory):
-    lst, table = [], []
+    files_content, table = [], []
     files = glob.glob('{}*.csv'.format(directory))
     for file in files:
         if file=="res.csv": continue
         f = open(file, "r")
-        lst += f.readlines()[1:]
+        files_content += f.readlines()[1:]
         f.close()
-    for line in lst:
+    for line in files_content:
+        if line[0]=='''"''':
+            i = line[1:].index('"')+2
+            name = line[:i]
+            normal_name=''
+            for symb in name:
+                if symb=='"' or symb==",": continue
+                normal_name+=symb
+            line = normal_name+line[i:]
         table.append(line.strip('\n').split(','))
     for i in range(len(table)):
         for j in range(len(table[i])):
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Lab2_Outp import outp
 from Lab2_GetWinners import GetWinners
 
 directory = input("Enter the directory: ")
-if directory == None: directory = "C:\\Users\\User\Desktop\Навчання\ОП\Lab_2\\"
+if directory == None: directory = "C:\\Users\\User\\Desktop\\Навчання\\ОП\\Lab_2\\"
 table = get_matrix(directory)
 rating = [(l[0], 0) for l in table]     #I'm using array generator, don't warry 'bout)
 GetWinners(rating, table)
